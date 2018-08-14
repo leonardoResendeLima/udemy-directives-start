a636baa15115f6a4123f132f3ac5f56ee405a364
Leonardo Resende Corrêa Lima <leonardoresendelima@gmail.com>
2018-08-14 11:14:38 -0300

    Directives setting values from outside

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,14 +1,29 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
 	selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+	@Input() defaultColor: string = 'transparent';
+	@Input('appBetterHighlight') highlightColor: string = 'blue';
+
+	@HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
 
 	constructor(private elRef: ElementRef, private rendered: Renderer2) { }
 
 	ngOnInit() {
-		this.rendered.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+		this.backgroundColor = this.defaultColor;
+		//this.rendered.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+	}
+
+	@HostListener('mouseenter') mouseover(eventData: Event) {
+		// this.rendered.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+		this.backgroundColor = this.highlightColor;
+	}
+
+	@HostListener('mouseleave') mouseleave(eventData: Event) {
+		//this.rendered.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+		this.backgroundColor = this.defaultColor;
 	}
 
 }
